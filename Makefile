8138b922ade547642ca160267b37513ed2eef9a9
Robert Durica <r.durica@gmail.com>
2022-08-15 22:13:38 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,12 @@
+#!/bin/bash
+repository = portfolio
+tag = 0.1.0
+platform = linux/amd64,linux/arm64
+
+build:
+	poetry export --without-hashes --output requirements.txt
+	docker build -t $(repository) .
+
+publish:
+	poetry export --without-hashes --output requirements.txt
+	docker buildx build --platform $(platform) --push -t docker.robbyte.net/$(repository):$(tag) .
---
+++ config/gunicorn.py
@@ -0,0 +1,17 @@
+# Gunicorn production config file"""
+
+# Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
+wsgi_app = "config.wsgi:application"
+# The granularity of Error log outputs
+loglevel = "info"
+# The number of worker processes for handling requests
+workers = 2
+# The socket to bind
+bind = "0.0.0.0:8000"
+# Redirect stdout/stderr to log file
+capture_output = True
+# PID file so you can easily fetch process ID
+pidfile = "/var/run/weatherkh.pid"
+# Write access and error info to /var/log
+accesslog = "/var/log/portfolio-access.log"
+errorlog = "/var/log/portfolio-error.log"
