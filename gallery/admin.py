c5f9222026a16af68919032095a1093109c7456b
Robert Durica <r.durica@gmail.com>
2022-08-27 22:20:06 +0200

    Added: Gallery model

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from gallery.models import Item
+
+
 # Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "url",
+        "is_enabled",
+    ]
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
+from ast import mod
+from statistics import mode
+
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    name: str = models.CharField(max_length=100)
+    description: str = models.TextField()
+    url: str = models.URLField(blank=True)
+    is_enabled: bool = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.name
