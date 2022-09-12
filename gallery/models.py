25c2a2926230a9bc3a36e8b4e10f1f4d05717f62
Robert Durica <r.durica@gmail.com>
2022-09-12 21:50:28 +0200

    Fixed: Limited description length for gallery

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     title: str = models.CharField(max_length=100)
-    description: str = models.TextField()
+    description: str = models.TextField(max_length=215)
     url: str = models.URLField(blank=True)
     image = models.ImageField(upload_to="gallery", blank=True)
     is_enabled: bool = models.BooleanField(default=False)
